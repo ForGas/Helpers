48546907454b0e50aea9debfb6a34b1ad72708fe
ForGas <55873291+ForGas@users.noreply.github.com>
2022-02-10 11:03:30 +0600

    Add: DisplayService

---
+++ DisplayService/DisplayConsole.cs
@@ -0,0 +1,28 @@
+﻿using DisplayService.Interfaces;
+
+namespace DisplayService
+{
+    public class DisplayConsole : IDisplay
+    {
+        public void Print<T>(ICollection<T> models) where T : class
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\t\t\t Start");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            var properties = models.AsQueryable().ElementType.GetProperties().ToList();
+            var entities = models.ToList();
+
+            entities.ForEach(entry =>
+            {
+                properties.ForEach(property =>
+                {
+                    Console.Write($"{property.Name} - ");
+                    Console.WriteLine($"{entry.GetType().GetProperty(property.Name)!.GetValue(entry, null)}    ");
+                });
+
+                Console.WriteLine();
+            });
+        }
+    }
+}
---
+++ DisplayService/Interfaces/IDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayService.Interfaces
+{
+    public interface IDisplay
+    {
+        public void Print<T>(ICollection<T> entities) where T : class;
+    }
+}
