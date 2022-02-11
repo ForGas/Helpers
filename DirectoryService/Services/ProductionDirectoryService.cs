e38a70ed3425aebd9a601f022cdb85a7ce723769
ForGas <55873291+ForGas@users.noreply.github.com>
2022-02-11 12:37:39 +0600

    Refactoring: minor

--- DirectoryService/Services/ProductionDirectoryService.cs
+++ DirectoryService/Services/ProductionDirectoryService.cs
@@ -5,7 +5,7 @@ namespace Service.Directory
     public class ProductionDirectoryService<Format> : BaseDirectoryService<Format>
         where Format : Enum
     {
-        public ProductionDirectoryService(string filesDirectoryName)
-            : base(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory), filesDirectoryName) { }
+        public ProductionDirectoryService(string fileDirectoryName)
+            : base(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory), fileDirectoryName) { }
     }
 }
